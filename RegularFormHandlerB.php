a640ef193b2e9edb580260e6cff4dab9e7e20d5d
Ervin Rudakov <ervin_rudakov@mail.ru>
2022-12-09 14:59:06 +0200

    Regular lab "b" done

--- RegularFormHandlerB.php
+++ RegularFormHandlerB.php
@@ -1,1 +1,9 @@
 <?php
+    $str=$_POST["str"];
+    preg_match_all('/\pL\d/',$str,$matches);
+    foreach ($matches[0] as &$value) {
+        $c = (int)($value[1]);
+        $c = $c * $c * $c;
+        $value=str_replace($value[1], strval($c), $value);
+        print($value);
+    }
